c59d59ab43ee6dfc10413fd109fbc75e6e8704a2
Nitin Soni <nitin.soni4@t-mobile.com>
2021-04-08 20:16:15 +0530

    added GalleryModule and Browse as development pods to test

---
+++ Africa/Pods/Target Support Files/Browse/Browse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Browse : NSObject
+@end
+@implementation PodsDummy_Browse
+@end
---
+++ Africa/Pods/Target Support Files/GalleryModule/GalleryModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GalleryModule : NSObject
+@end
+@implementation PodsDummy_GalleryModule
+@end
